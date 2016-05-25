5ba2bfd18616b04302a9156393e0d1a58f13173f
Maria Pilar Guerra Arias <pili@3scale.net>
2016-05-25 19:28:47 +0200

    Updates for #22

--- oauth2/resource-owner-password-flow/no-token-generation/get_token.lua
+++ oauth2/resource-owner-password-flow/no-token-generation/get_token.lua
@@ -36,9 +36,7 @@ end
 -- Check valid params ( client_id / secret / redirect_url, whichever are sent) against 3scale
 function check_client_credentials(params)
   local res = ngx.location.capture("/_threescale/check_credentials",
-              { args=( params.client_id and "app_id="..params.client_id.."&" or "" )..
-                     ( params.client_secret and "app_key="..params.client_secret.."&" or "" )..
-                     ( params.redirect_uri and "redirect_uri="..params.redirect_uri or "" )})
+              { args = { app_id = params.client_id or nil, app_key = params.client_secret or nil, redirect_uri = params.redirect_uri or nil } })
   
   if res.status ~= 200 then   
     ngx.status = 401
